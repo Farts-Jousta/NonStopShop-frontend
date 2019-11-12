afb21edefe1f10f450ce237696ea636483beef4d
Farts-Jousta <49703765+Farts-Jousta@users.noreply.github.com>
2019-11-12 21:26:09 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Mainpage />
+        <Orders />
+        <Search />
+      </div>
+    );
+  }
+}
+
+export default App;
